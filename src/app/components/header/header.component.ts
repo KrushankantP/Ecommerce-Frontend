9baf4a8c3d2a5779ba02cb3b2b63aa6f3f714e82
Patel Krushankant <patelkk1618@gmail.com>
2020-08-15 10:31:07 -0500

    YourCart - Header component.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {cartModelServer} from "../../models/cart.model";
+import {CartService} from "../../services/cart.service";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  cartData: cartModelServer;
+  cartTotal: number;
+
+  constructor(public _cartService: CartService) { }
 
   ngOnInit(): void {
+    this._cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+
+    this._cartService.cartData$.subscribe(data=> this.cartData = data);
   }
 
 }
