3fc11c41a1d7f5b4733588932fafd562de034318
Patel Krushankant <patelkk1618@gmail.com>
2020-08-16 08:10:09 -0500

    Thank you Page done.

--- src/app/components/thankyou/thankyou.component.ts
+++ src/app/components/thankyou/thankyou.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {OrderService} from "../../services/order.service";
 
 @Component({
   selector: 'app-thankyou',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thankyou.component.scss']
 })
 export class ThankyouComponent implements OnInit {
+  message: String;
+  orderId: Number;
+  products;
+  cartTotal;
+  constructor(private _router: Router,
+              private _orderService: OrderService) {
 
-  constructor() { }
+    const navigation =this._router.getCurrentNavigation();
+    const state = navigation.extras.state as {
+      message:string,
+      products: ProductResponseModel[],
+      orderId: number,
+      total: number
+    }
+    this.message = state.message;
+    this.orderId = state.orderId;
+    this.products = state.products;
+    this.cartTotal = state.total;
+    console.log(this.products);
+  }
+
 
   ngOnInit(): void {
   }
 
+}
+interface ProductResponseModel {
+  id: Number;
+  title: String;
+  description: String;
+  price: Number;
+  quantityOrdered: Number;
+  image: String;
 }
